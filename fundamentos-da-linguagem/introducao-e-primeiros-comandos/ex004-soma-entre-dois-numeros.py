0ce3816e9259c002e43d6039f7865a1b6ea69e56
Guilherme Leal <guilhermebatistaleal07@gmail.com>
2026-02-26 11:51:24 -0300

    Exercício de soma entre dois números com conversão para float

---
+++ fundamentos-da-linguagem/introducao-e-primeiros-comandos/ex004-soma-entre-dois-numeros.py
@@ -0,0 +1,14 @@
+"""
+-- Soma entre dois números
+
+Objetivo:
+Ler dois valores numéricos informados pelo usuário,
+converter para tipo numérico e exibir a soma.
+"""
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+soma = n1 + n2
+
+print("A soma entre {} e {} é igual a {}".format(n1, n2, soma))
