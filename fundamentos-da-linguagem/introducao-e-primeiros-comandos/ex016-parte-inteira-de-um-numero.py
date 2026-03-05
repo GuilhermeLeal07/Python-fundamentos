d9fdb508ecb7ae6510e0178490a41286e03a91b7
Guilherme Leal <guilhermebatistaleal07@gmail.com>
2026-03-05 19:15:08 -0300

    Renomeia variável para cálculo da parte inteira

---
+++ fundamentos-da-linguagem/introducao-e-primeiros-comandos/ex016-parte-inteira-de-um-numero.py
@@ -0,0 +1,18 @@
+"""
+Parte Inteira de um Número
+
+Objetivo:
+Ler um número real e mostrar apenas a parte inteira desse número.
+"""
+
+# Entrada de dados
+n = float(input("Digite um número: "))
+
+# Cálculo da parte inteira
+parte_inteira = int(n)
+
+# Saída de dados
+print("-" * 40)
+print("O número digitado foi {}".format(n))
+print("A parte inteira desse número é {}".format(parte_inteira))
+print("-" * 40)
