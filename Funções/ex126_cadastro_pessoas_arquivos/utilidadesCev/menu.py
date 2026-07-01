3650f14be8c05cf7d2f73e7fafd768c96e68e127
Guilherme Leal <guilhermebatistaleal07@gmail.com>
2026-07-01 12:30:44 -0300

    Refactor menu.py for improved readability

--- Funções/ex126_cadastro_pessoas_arquivos/utilidadesCev/menu.py
+++ Funções/ex126_cadastro_pessoas_arquivos/utilidadesCev/menu.py
@@ -1,4 +1,9 @@
-from funcoes import arquivo_existe, criar_arquivo, ler_arquivo, cadastrar
+from funcoes import (
+    arquivo_existe,
+    criar_arquivo,
+    ler_arquivo,
+    cadastrar,
+)
 
 ARQUIVO = 'arquivo.txt'
 
@@ -6,21 +11,28 @@ if not arquivo_existe(ARQUIVO):
     criar_arquivo(ARQUIVO)
 
 while True:
+
     print('─' * 30)
-    print('  MENU PRINCIPAL')
+    print('      MENU PRINCIPAL')
     print('─' * 30)
+
     print('1 - Ver pessoas cadastradas')
     print('2 - Cadastrar nova pessoa')
     print('0 - Sair')
+
     opc = input('Sua opção: ').strip()
+
     if opc == '1':
         ler_arquivo(ARQUIVO)
+
     elif opc == '2':
         nome = input('Nome: ')
         idade = input('Idade: ')
         cadastrar(ARQUIVO, nome, idade)
+
     elif opc == '0':
         print('Saindo do programa...')
         break
+
     else:
         print('ERRO! Digite uma opção válida.')
