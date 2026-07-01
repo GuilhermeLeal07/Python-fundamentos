11509d92bb0702fc90fff99eeadc7b019abb6b92
Guilherme Leal <guilhermebatistaleal07@gmail.com>
2026-07-01 12:28:56 -0300

    Add main menu for person registration system

---
+++ Funções/ex126_cadastro_pessoas_arquivos/utilidadesCev/menu.py
@@ -0,0 +1,26 @@
+from funcoes import arquivo_existe, criar_arquivo, ler_arquivo, cadastrar
+
+ARQUIVO = 'arquivo.txt'
+
+if not arquivo_existe(ARQUIVO):
+    criar_arquivo(ARQUIVO)
+
+while True:
+    print('─' * 30)
+    print('  MENU PRINCIPAL')
+    print('─' * 30)
+    print('1 - Ver pessoas cadastradas')
+    print('2 - Cadastrar nova pessoa')
+    print('0 - Sair')
+    opc = input('Sua opção: ').strip()
+    if opc == '1':
+        ler_arquivo(ARQUIVO)
+    elif opc == '2':
+        nome = input('Nome: ')
+        idade = input('Idade: ')
+        cadastrar(ARQUIVO, nome, idade)
+    elif opc == '0':
+        print('Saindo do programa...')
+        break
+    else:
+        print('ERRO! Digite uma opção válida.')
